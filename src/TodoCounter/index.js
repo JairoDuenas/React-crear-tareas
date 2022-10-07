5244b852ad34f0a38e6bc4b3433530bd56ae391e
JairoDuenas <jairoduenas.ing@gmail.com>
2022-10-07 10:54:18 -0500

    Agregando logo y stilos

--- src/TodoCounter/index.js
+++ src/TodoCounter/index.js
@@ -1,10 +1,23 @@
 import React from "react";
 import './TodoCounter.css';
+import logo from "../statics/logo.png"
 
 function TodoCounter({ totalTodos, completedTodos, loading }) {
   return (
-    <h2 className={`TodoCounter ${!!loading && "TodoCounter--loading"}`} >Has completado {completedTodos} de {totalTodos} tareas
-    </h2>
+    <>
+      <div className="logo">
+        <img 
+        src={logo}
+        alt="logo-personal"
+        />
+      </div>
+      <h2
+        className={
+          `TodoCounter ${!!loading && "TodoCounter--loading"}`}
+      >
+        Has completado {completedTodos} de {totalTodos} tareas
+      </h2>
+    </>
   );
 }
 
